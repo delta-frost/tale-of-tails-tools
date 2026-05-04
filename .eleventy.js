810b06c0ea48671d0ec7f979dd75fd699ec8a4e9
delta-frost <badwilds@gmail.com>
2026-05-04 16:42:24 -0500

    Add Github Actions deploy + fix paths for project site

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,15 @@
 module.exports = function(eleventyConfig) {
-  // Copy all static folders straight to output
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("scripts");
   eleventyConfig.addPassthroughCopy("assets");
-  // Keep these files too
   eleventyConfig.addPassthroughCopy("LICENSE");
   eleventyConfig.addPassthroughCopy("README.md");
+
   return {
+    pathPrefix: "/tale-of-tails-tools/",   // ← add this
     dir: {
-      input: ".",        // where your source files are now
-      output: "_site"    // where the built site goes
+      input: ".",
+      output: "_site"
     }
   };
 };
